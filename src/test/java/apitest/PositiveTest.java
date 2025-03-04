c8cf8bbcc16b0d3d442f011c41e375a902cac2d6
martin-edu <37124774+martin-edu@users.noreply.github.com>
2025-03-03 19:37:47 -0600

    Update PositiveTest.java
    Adding http status class to PositiveTest class

--- src/test/java/apitest/PositiveTest.java
+++ src/test/java/apitest/PositiveTest.java
@@ -2,28 +2,28 @@ package apitest;
 
 import domain.Post;
 import io.restassured.response.Response;
+
+import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
 public class PositiveTest extends TestBase {
-	
-	/**
-	 * @Description This is a get test for all post 
-	 */
+			
+   /**
+   * @Description This is a get test for all post 
+   */
     @Test
-    public void testGetAllPosts() {
-    	System.out.println("Test positive thread number "+Thread.currentThread().getId());
+    public void getAllPosts() {
         Response response = postApiClient.getAllPosts();
-        //Post targetObject = new Gson().fromJson(response.body, Post.class);
-        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
         Assert.assertNotNull(response, "The response is null");
     }
     
     /**
-	 * @Description This is a get test by ID for post resource
-	 */
+    * @Description This is a get test by ID for post resource
+    */
     @Test
     public void getPostById()
     {
@@ -31,7 +31,7 @@ public class PositiveTest extends TestBase {
     	Post post = response.as(Post.class);
     	
     	Assert.assertEquals(post.getId(), 1);
-    	Assert.assertEquals(response.getStatusCode(), 200);
+    	Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
     	Assert.assertNotNull(response, "The response is null");
     	Assert.assertEquals(post.getBody(), "quia et suscipit\nsuscipit "
     			+ "recusandae consequuntur expedita et cum\nreprehenderit "
@@ -40,40 +40,37 @@ public class PositiveTest extends TestBase {
     }
     
     /**
-	 * @Description This is a get test by ID for comment
-	 */
+    * @Description This is a get test by ID for comment
+    */
     @Test
-    public void getElementCommentsById()
+    public void getPostCommentsById()
 	{
-		//Post[] posts = get(url+"/posts/1/comments").as(Post[].class);
     	Response response = commentApiClient.getCommentsByPostId(1);
-    	Assert.assertEquals(response.getStatusCode(), 200);
+    	Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
     	Assert.assertNotNull(response, "The response is null");
-			
 	}
     
     /**
-	 * @Description This is a get test by ID for comments, parameter inside URL
-	 */
+    * @Description This is a get test by ID for comments, parameter inside URL
+    */
     @Test
-    public void getElementCommentsByIdWhitUrlParameter()
+    public void getPostCommentsByIdWhitUrlParameter()
 	{
     	Response response = commentApiClient.getCommentsByPostIdWithUrlParameter(1);
-    	Assert.assertEquals(response.getStatusCode(), 200);
-    	Assert.assertNotNull(response, "The response is null");
-			
+    	Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
+    	Assert.assertNotNull(response, "The response is null");	
 	}
     
     /**
-	 * @Description This is a post test for create a new Post
-	 */
+    * @Description This is a post test for create a new Post
+    */
     @Test
-    public void testCreatePost() {
+    public void createPost() {
         Post newPost = new Post(1, 1,  "New Title", "New Body");
         Response response = postApiClient.createPost(newPost);
         Post postResponse = response.as(Post.class);
         
-        Assert.assertEquals(response.getStatusCode(), 201);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_CREATED);
     	Assert.assertNotNull(response, "The response is null");
     	Assert.assertEquals(postResponse.getUserId(), newPost.getUserId());
     	Assert.assertEquals(postResponse.getTitle(), newPost.getTitle());
@@ -82,17 +79,16 @@ public class PositiveTest extends TestBase {
     }
     
     /**
-	 * @Description This is a put test for update a new Post
-	 */
+    * @Description This is a put test for update a new Post
+    */
     @Test
-	void putAPostById()
+	void putPostById()
 	{
         Post newPost = new Post(1, 1,  "New Title", "New Body");
-    	
     	Response response = postApiClient.updatePost(1, newPost);
     	Post postResponse = response.as(Post.class);
     	 
-    	Assert.assertEquals(response.getStatusCode(), 200);
+    	Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
      	Assert.assertNotNull(response, "The response is null");
         Assert.assertEquals(postResponse.getUserId(), newPost.getUserId());
      	Assert.assertEquals(postResponse.getTitle(), newPost.getTitle());
@@ -101,10 +97,10 @@ public class PositiveTest extends TestBase {
 	}
     
     /**
-	 * @Description This is a patch test for patch a new Post
-	 */
+    * @Description This is a patch test for patch a new Post
+    */
     @Test
-	void patchAPostById()
+	void patchPostById()
 	{
 		HashMap post = new HashMap();
 		
@@ -116,21 +112,20 @@ public class PositiveTest extends TestBase {
 		
 		
 		
-		Assert.assertEquals(response.getStatusCode(), 200);
+		Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
      	Assert.assertNotNull(response, "The response is null");
         Assert.assertEquals(postResponse.getUserId(), 1);
      	Assert.assertEquals(postResponse.getTitle(), "Updating api element");
 	}
     
     /**
-	 * @Description This is a delete test by id
-	 */
+    * @Description This is a delete test by id
+    */
     @Test
-    public void testDeletePost() {
+    public void deletePost() {
         Response response = postApiClient.deletePost(1);
-        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
         Assert.assertNotNull(response, "The response is null");
-        Assert.assertNotNull(response, "{}");
-        
+        Assert.assertNotNull(response, "{}");   
     }
 }
